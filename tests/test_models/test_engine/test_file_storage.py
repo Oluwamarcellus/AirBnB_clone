84ba137db362848531251cc30f24d0dd91160e3b
Ayowale <haydexwisdom@gmail.com>
2023-08-13 20:03:38 +0100

    Updated test cases

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,7 +7,6 @@ Test for FileStorage module
 import os
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-import datetime
 import unittest
 import models
 from models.amenity import Amenity
@@ -66,21 +65,68 @@ class TestStorage(unittest.TestCase):
         """
         Testing the new() method
         """
-        sample = City()
-        models.storage.new(sample)
-        self.assertIn("City." + sample.id, models.storage.all().keys())
-        self.assertIn(sample, models.storage.all().values())
+        city = City()
+        state = State()
+        amenity = Amenity()
+        user = User()
+        review = Review()
+        place = Place()
+        base = BaseModel()
+        self.assertIn("City." + city.id, models.storage.all().keys())
+        self.assertIn("State." + state.id, models.storage.all().keys())
+        self.assertIn("Amenity." + amenity.id, models.storage.all().keys())
+        self.assertIn("User." + user.id, models.storage.all().keys())
+        self.assertIn("Review." + review.id, models.storage.all().keys())
+        self.assertIn("Place." + place.id, models.storage.all().keys())
+        self.assertIn("BaseModel." + base.id, models.storage.all().keys())
+        self.assertIn(city, models.storage.all().values())
+        self.assertIn(state, models.storage.all().values())
+        self.assertIn(amenity, models.storage.all().values())
+        self.assertIn(user, models.storage.all().values())
+        self.assertIn(review, models.storage.all().values())
+        self.assertIn(place, models.storage.all().values())
+        self.assertIn(base, models.storage.all().values())
 
     def test_save(self):
         """
         Testing the save() method
         """
-        sample = City()
-        models.storage.new(sample)
+        city = City()
+        state = State()
+        amenity = Amenity()
+        user = User()
+        review = Review()
+        place = Place()
+        base = BaseModel()
         models.storage.save()
         with open("file.json", "r") as file:
             f_contents = file.read()
-            self.assertIn("City." + sample.id, f_contents)
+            self.assertIn("City." + city.id, f_contents)
+            self.assertIn("State." + state.id, f_contents)
+            self.assertIn("Amenity." + amenity.id, f_contents)
+            self.assertIn("User." + user.id, f_contents)
+            self.assertIn("Review." + review.id, f_contents)
+            self.assertIn("Place." + place.id, f_contents)
+            self.assertIn("BaseModel." + base.id, f_contents)
+
+
+class TestTypes(unittest.TestCase):
+    """
+    Test fot instances types
+    """
+
+    def test_file_storage_inst(self):
+        """
+        Testing FS inst type
+        """
+        fs = FileStorage()
+        self.assertEqual(type(fs), FileStorage)
+
+    def test_storage(self):
+        """
+        Testing storage inst
+        """
+        self.assertEqual(type(models.storage), FileStorage)
 
 
 if __name__ == "__main__":
